83f840e4e4864608dbc067217fc158744068cb1e
Kristin Esterley <Kristin@Kristins-MBP.attlocal.net>
2017-03-16 11:00:58 -0400

    jaws

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection.mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
   password: "root",
   database: "burgers_db"
 });
-
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
